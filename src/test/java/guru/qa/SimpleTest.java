cecbd476c75cbbfe6ee83dad575fde2eecc34271
SlavovaDaria <slavova19@gmail.com>
2022-05-08 16:56:14 +0300

    added before each

--- src/test/java/guru/qa/SimpleTest.java
+++ src/test/java/guru/qa/SimpleTest.java
@@ -1,4 +1,25 @@
 package guru.qa;
 
+import org.junit.jupiter.api.*;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+
 public class SimpleTest {
+
+    @BeforeEach
+    void openYaPage() {
+        System.out.println("###     @BeforeEach");
+//        Selenide.open("https://ya.ru");
+    }
+
+    @Test
+    void assertTest() {
+
+    }
+
+    @Test
+    void assertTest1() {
+
+    }
+
 }
