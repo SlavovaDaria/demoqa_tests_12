18fe117fc2e07dd20d976f6b5f4d0a24efc5e422
SlavovaDaria <slavova19@gmail.com>
2022-05-08 19:13:26 +0300

    changed url

--- src/test/java/guru/qa/TextBoxTests.java
+++ src/test/java/guru/qa/TextBoxTests.java
@@ -1,19 +1,23 @@
 package guru.qa;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.*;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import com.codeborne.selenide.Configuration;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TextBoxTests {
 
     @BeforeAll
     static void setUp() {
         Configuration.holdBrowserOpen = true;
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1024x768";
+        Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
+
     }
 
 
