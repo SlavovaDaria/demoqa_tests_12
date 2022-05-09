19a54c1fb065f449d32084c10f5054104ad3ae41
SlavovaDaria <slavova19@gmail.com>
2022-05-09 10:03:29 +0300

    checkouts for main areas are made

--- src/test/java/guru/qa/HomeworkTests.java
+++ src/test/java/guru/qa/HomeworkTests.java
@@ -27,16 +27,23 @@ public class HomeworkTests {
         $("[id=firstName]").setValue("First Name");
         $("[id=lastName]").setValue("Last Name");
         $("[id=userEmail]").setValue("name@example.com");
-        $(byText("Male")).click();
+        $(byText("Other")).click();
         $("[id=userNumber]").setValue("0123456789");
-
         $("[id=currentAddress]").setValue("Current Address");
-
         $("[id=submit]").click();
 
-
-
-
-
+        $("[id=example-modal-sizes-title-lg]").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").$(byText("First Name Last Name"))
+                .parent().shouldHave(text("First Name Last Name"));
+        $(".table-responsive").$(byText("name@example.com"))
+                .parent().shouldHave(text("name@example.com"));
+        $(".table-responsive").$(byText("Other"))
+                .parent().shouldHave(text("Other"));
+        $(".table-responsive").$(byText("0123456789"))
+                .parent().shouldHave(text("0123456789"));
+        $(".table-responsive").$(byText("09 May,2022"))
+                .parent().shouldHave(text("09 May,2022"));
+        $(".table-responsive").$(byText("Current Address"))
+                .parent().shouldHave(text("Current Address"));
     }
 }
