8013a0bc4e8d301f410d7b8f0b374ef79897b5f8
SlavovaDaria <slavova19@gmail.com>
2022-05-09 14:59:45 +0300

    final commit - all tests are added

--- src/test/java/guru/qa/HomeworkTests.java
+++ src/test/java/guru/qa/HomeworkTests.java
@@ -24,15 +24,26 @@ public class HomeworkTests {
 
         open("/automation-practice-form");
 
-        $("[id=firstName]").setValue("First Name");
-        $("[id=lastName]").setValue("Last Name");
-        $("[id=userEmail]").setValue("name@example.com");
+        $("#firstName").setValue("First Name");
+        $("#lastName").setValue("Last Name");
+        $("#userEmail").setValue("name@example.com");
         $(byText("Other")).click();
-        $("[id=userNumber]").setValue("0123456789");
-        $("[id=currentAddress]").setValue("Current Address");
-        $("[id=submit]").click();
+        $("#userNumber").setValue("0123456789");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("September");
+        $(".react-datepicker__year-select").selectOption("1991");
+        $("[aria-label$='September 4th, 1991']").click();
+        $("#subjectsInput").setValue("C").pressEnter();
+        $(byText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("img/1.jpg");
+        $("#currentAddress").setValue("Current Address");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
 
-        $("[id=example-modal-sizes-title-lg]").shouldHave(text("Thanks for submitting the form"));
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").$(byText("First Name Last Name"))
                 .parent().shouldHave(text("First Name Last Name"));
         $(".table-responsive").$(byText("name@example.com"))
@@ -41,9 +52,17 @@ public class HomeworkTests {
                 .parent().shouldHave(text("Other"));
         $(".table-responsive").$(byText("0123456789"))
                 .parent().shouldHave(text("0123456789"));
-        $(".table-responsive").$(byText("09 May,2022"))
-                .parent().shouldHave(text("09 May,2022"));
+        $(".table-responsive").$(byText("04 September,1991"))
+                .parent().shouldHave(text("04 September,1991"));
+        $(".table-responsive").$(byText("Physics"))
+                .parent().shouldHave(text("Physics"));
+        $(".table-responsive").$(byText("Reading"))
+                .parent().shouldHave(text("Reading"));
+        $(".table-responsive").$(byText("1.jpg"))
+                .parent().shouldHave(text("1.jpg"));
         $(".table-responsive").$(byText("Current Address"))
                 .parent().shouldHave(text("Current Address"));
+        $(".table-responsive").$(byText("NCR Delhi"))
+                .parent().shouldHave(text("NCR Delhi"));
     }
 }
