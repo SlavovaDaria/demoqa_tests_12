f9f6a2df7c6d5da2a0679b9fd47132319ea262c8
Daria-bot-coder <93128721+Daria-bot-coder@users.noreply.github.com>
2022-06-12 18:09:04 +0300

    initcommit

--- src/test/java/guru/qa/HomeworkTests.java
+++ src/test/java/guru/qa/HomeworkTests.java
@@ -1,117 +1,32 @@
 package guru.qa;
 
-import com.codeborne.pdftest.PDF;
-import com.codeborne.pdftest.matchers.ContainsExactText;
-import org.junit.jupiter.api.Assertions;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Enumeration;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-import java.util.zip.ZipInputStream;
-
-import static org.hamcrest.MatcherAssert.assertThat;
+import static com.codeborne.selenide.Condition.exactTextCaseSensitive;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class HomeworkTests {
-    public static String resourceName = "src/test/resources/ziplesson.zip";
-    public static String resourceZipName = "ziplesson.zip";
 
-    public static String textPdfName = "example.pdf";
-    public static String textXlsxName = "import_ou_xlsx.xlsx";
-    public static String textCsvName = "import_ou_csv.csv";
-    public static File file = new File(resourceName);
-    static ClassLoader cl = HomeworkTests.class.getClassLoader();
+    public static final ElementsCollection PAGES = $("#wiki-pages-box").$$("ul li");
+    public static final SelenideElement WIKI_TAB = $("#wiki-tab");
+    public static final SelenideElement WIKI_PAGES_FILTER = $("#wiki-pages-filter");
+    public static final SelenideElement WIKI_BODY = $("#wiki-body");
 
-    static void zipFindTest(String findText) {
-        try {
-            ZipFile sourceZipFile = new ZipFile(file);
-            String searchFileName = findText;
-            Enumeration e = sourceZipFile.entries();
-            boolean found = false;
-            System.out.println("Trying to search " + searchFileName + " in " + sourceZipFile.getName());
-            while (e.hasMoreElements()) {
-                ZipEntry entry = (ZipEntry) e.nextElement();
-
-                if (entry.getName().indexOf(searchFileName) != -1) {
-                    found = true;
-                    System.out.println("Found " + entry.getName());
-
-                }
-            }
-
-            if (found == false) {
-                System.out.println("File " + searchFileName + " Not Found inside ZIP file " + sourceZipFile.getName());
-            }
-
-            sourceZipFile.close();
-        } catch (IOException ioe) {
-            System.out.println("Error opening zip file" + ioe);
-        }
-    }
-
-    @Test
-    void zipParsingTest() throws Exception {
-        ZipFile zf = new ZipFile(file);
-        ZipInputStream is = new ZipInputStream(cl.getResourceAsStream(resourceZipName));
-        ZipEntry entry;
-        while ((entry = is.getNextEntry()) != null) {
-            zipFindTest(entry.getName());
-            System.out.println(String.format(
-                    "Item: %s \nType: %s \nSize: %d\n",
-                    entry.getName(),
-                    entry.isDirectory() ? "directory" : "file",
-                    entry.getSize()
-            ));
-        }
-    }
-
-    @Test
-    void zipPdfTest() throws Exception {
-        ZipFile zf = new ZipFile(file);
-        ZipInputStream is = new ZipInputStream(cl.getResourceAsStream(resourceZipName));
-        ZipEntry entry;
-        while ((entry = is.getNextEntry()) != null) {
-            try (InputStream inputStream = zf.getInputStream(entry)) {
-                if (entry.getName().equals(textPdfName)) {
-                    PDF pdf = new PDF(inputStream);
-                    Assertions.assertEquals(1, pdf.numberOfPages);
-                    assertThat(pdf, new ContainsExactText("PDF"));
-                    System.out.println("example.pdf found and checked success");
-                }
-            }
-        }
-    }
+    public static final String SEARCH_FOR_PAGE = "SoftAssertions";
+    public static final String JUNIT_5_BLOCK = "3. Using JUnit5 extend test class:";
 
 
     @Test
-    void zipXlsTest() throws Exception {
-        ZipFile zf = new ZipFile(file);
-        ZipInputStream is = new ZipInputStream(cl.getResourceAsStream(resourceZipName));
-        ZipEntry entry;
-        while ((entry = is.getNextEntry()) != null) {
-            try (InputStream inputStream = zf.getInputStream(entry)) {
-                if (entry.getName().equals(textXlsxName)) {
-                    System.out.println("import_ou_xlsx.xlsx found and checked success");
-                }
-            }
-        }
-    }
-
-
-    @Test
-    void zipCsvTest() throws Exception {
-        ZipFile zf = new ZipFile(file);
-        ZipInputStream is = new ZipInputStream(cl.getResourceAsStream(resourceZipName));
-        ZipEntry entry;
-        while ((entry = is.getNextEntry()) != null) {
-            try (InputStream inputStream = zf.getInputStream(entry)) {
-                if (entry.getName().equals(textCsvName)) {
-                    System.out.println("import_ou_csv.csv found and checked success");
-                }
-            }
-        }
+    void checkSoftAssertionsPage() {
+        open("https://github.com/selenide/selenide");
+        WIKI_TAB.click();
+        WIKI_PAGES_FILTER.setValue(SEARCH_FOR_PAGE);
+        PAGES.findBy(exactTextCaseSensitive(SEARCH_FOR_PAGE)).click();
+        WIKI_BODY.$(byText(JUNIT_5_BLOCK)).shouldBe(visible);
     }
 }
