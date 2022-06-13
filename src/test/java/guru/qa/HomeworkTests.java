d37fea8b684a370730c163243312dc9cf276184c
Daria-bot-coder <93128721+Daria-bot-coder@users.noreply.github.com>
2022-06-13 16:45:16 +0300

    jpg instead of png done

--- src/test/java/guru/qa/HomeworkTests.java
+++ src/test/java/guru/qa/HomeworkTests.java
@@ -34,7 +34,7 @@ public class HomeworkTests extends TestBase {
             $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
             $("#subjectsInput").setValue("Math").pressEnter();
             $("#hobbiesWrapper").$(byText("Sports")).click();
-            $("#uploadPicture").uploadFromClasspath("img/1.png");
+            $("#uploadPicture").uploadFromClasspath("img/1.jpg");
             $("#currentAddress").setValue("Some street 1");
             $("#state").click();
             $("#stateCity-wrapper").$(byText("NCR")).click();
